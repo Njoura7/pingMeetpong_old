bce2af01da977ba8b9762c3b5ca7b12bfaf16b94
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2023-08-15 13:59:29 +0200

    fix typo in model/Users.js 2 xd

--- server/mongodb/models/Users.js
+++ server/mongodb/models/Users.js
@@ -1,19 +1,18 @@
 import mongoose from "mongoose"
 
-const userSchema=mongoose.Schema({
-   /* I will have to add constraints for _id when adding new users to make them unique*/
-   username:String,
-   password:String,
-   reviews:[{body:String,date:Date}],
-   record:{
-      Wins:Number,
-      Losses:Number
-   },
-   matchHistory:String,
-   availabilty:Boolean
+const userSchema = mongoose.Schema({
+  /* I will have to add constraints for _id when adding new users to make them unique*/
+  username: String,
+  password: String,
+  reviews: [{ body: String, date: Date }],
+  record: {
+    Wins: Number,
+    Losses: Number,
+  },
+  matchHistory: String,
+  availability: Boolean,
 })
 
-const User=mongoose.model('User',userSchema);
+const User = mongoose.model("User", userSchema)
 
-
-export default User;
+export default User
