e1d886ffac8472835c31d36dddd0ffcfbe5158b3
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2023-08-16 22:21:43 +0200

    add seach input in LookForPlayer page

--- client/src/pages/LookForPlayer.jsx
+++ client/src/pages/LookForPlayer.jsx
@@ -1,7 +1,29 @@
-import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
 
 function LookForPlayer() {
-  return <div>LookForPlayer</div>
+  return (
+    <>
+      <h1>Look For Player</h1>
+
+      <div className='search'>
+        <input
+          type='text'
+          className='searchTerm'
+          placeholder='Look for a player'
+        />
+        <button
+          type='submit'
+          className='searchButton'>
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            size='l'
+            style={{ color: "#ccc" }}
+          />
+        </button>
+      </div>
+    </>
+  )
 }
 
 export default LookForPlayer
