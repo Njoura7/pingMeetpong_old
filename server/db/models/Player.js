7be2995f7fffe92166e2ffcf731fe13b9e3f7f34
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2023-08-29 18:12:14 +0200

    fix some typos and change mongodb folder into db folder

--- server/db/models/Player.js
+++ server/db/models/Player.js
@@ -4,34 +4,34 @@ const userSchema = mongoose.Schema({
   username: {
     type: String,
     unique: true,
-    required:true
+    required: true,
   },
-  password: { 
+  password: {
     type: String,
-    required:true, 
-    minlength:5
-   },
+    required: true,
+    minlength: 5,
+  },
   reviews: {
-    type:Map,
-    of:String
+    type: Map,
+    of: String,
   },
   record: {
     Wins: Number,
     Losses: Number,
   },
-  matchJoined:{
-    type:String,
-    default:""  
+  matchJoined: {
+    type: String,
+    default: "",
   },
-  // //match history = array of objects , object {date , mode (1v1,2v2) , result(win or lose), score}         
+  // match history = array of objects , object {date , mode (1v1,2v2) , result(win or lose), score}
   // matchHistory: {
   //   type:String,
   //   default
   // }
   availability: {
-    type:Boolean,
-    default:true
-  }
+    type: Boolean,
+    default: true,
+  },
 })
 
 const Player = mongoose.model("Player", userSchema, "players")
